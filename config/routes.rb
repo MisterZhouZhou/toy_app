3baf2f0c559fa3ae63f5ac7b5fc0dcd3c9dd5671
MisterZhouZhou <m15038960016@163.com>
2017-07-25 14:40:04 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ToyApp::Application.routes.draw do
   resources :users
 
-  root 'users#index'
+  root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
