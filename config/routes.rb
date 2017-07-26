a1fe7bf3167a40590af4f4cf2a2e39be7b8855e2
MisterZhouZhou <m15038960016@163.com>
2017-07-26 14:09:46 +0800

    alert

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 ToyApp::Application.routes.draw do
+  root 'static_pages#home'
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   resources :microposts
 
   resources :users
 
-  root 'application#hello'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
