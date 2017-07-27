680701dc2025af4eb1467e6a70e800f1bb8d64d4
MisterZhouZhou <m15038960016@163.com>
2017-07-27 13:38:24 +0800

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        log_in @user
         flash[:success] = "Welcome to the Sample App!"
         format.html { redirect_to @user }
         format.json { render action: 'show', status: :created, location: @user }
